044cccad97bdb39843424f7670e496485d53493d
Михаил Никитин <misha@mrsunstrike.ru>
2023-06-10 10:42:43 +0700

    Добавил функцию валидации города

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import dateparser
+from geopy.geocoders import Nominatim
 
 def standardize_birthdate(date):
     '''Функция стандартизации даты рождения'''
@@ -7,3 +8,11 @@ def standardize_birthdate(date):
     # Приводим полученную дату к нужному формату "ДД.ММ.ГГГГ"
     formatted_date = birt_date.strftime('%d.%m.%Y')
     return formatted_date
+
+def validate_city(city):
+    geolocator = Nominatim(user_agent="my_app")
+    location = geolocator.geocode(city)
+    if location == None:
+        raise ValueError
+    else:
+        return city
